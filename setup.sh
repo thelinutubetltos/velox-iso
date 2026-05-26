7d6ce983be1aac0534938d96acce7c115ac2ea71
thelinutubetltos <thelinutubetltos@gmail.com>
2026-05-25 18:11:43 -0700

    fix: sudoers NOPASSWD + wrapper script to auto-launch Calamares

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 ############################################################
 # Author    : Your Name
-# Website   : https://github.com/YOURUSERNAME/velox-iso
+# Website   : https://github.com/thelinuxtltos/velox-iso
 ############################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
