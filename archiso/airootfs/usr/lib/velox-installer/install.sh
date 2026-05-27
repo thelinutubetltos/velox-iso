8a76ea230d8162ce17b0664e24e294e027f3a02b
thelinutubetltos <thelinutubetltos@gmail.com>
2026-05-26 18:13:53 -0700

    feat: add UEFI support and NVIDIA/AMD GRUB entries

--- archiso/airootfs/usr/lib/velox-installer/install.sh
+++ archiso/airootfs/usr/lib/velox-installer/install.sh
@@ -16,22 +16,52 @@ mkinitcpio -p linux
 ROOT_PART=$(findmnt -n -o SOURCE /)
 ROOT_DISK=$(lsblk -no pkname $ROOT_PART)
 ROOT_UUID=$(blkid -s UUID -o value $ROOT_PART)
-
 echo "ROOT_PART=$ROOT_PART ROOT_DISK=$ROOT_DISK ROOT_UUID=$ROOT_UUID"
 
-# Install grub to MBR
-grub-install --target=i386-pc /dev/$ROOT_DISK
+# Install grub - support both BIOS and UEFI
+if [ -d /sys/firmware/efi ]; then
+    mkdir -p /boot/efi
+    mount /dev/${ROOT_DISK}1 /boot/efi 2>/dev/null || true
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Velox --removable
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Velox
+else
+    grub-install --target=i386-pc /dev/$ROOT_DISK
+fi
 
 # Write grub.cfg
 mkdir -p /boot/grub
 cat > /boot/grub/grub.cfg << GRUBEOF
 set default=0
-set timeout=5
+set timeout=10
 
 menuentry "Velox Linux" {
     search --no-floppy --fs-uuid --set=root $ROOT_UUID
     linux /boot/vmlinuz-linux root=UUID=$ROOT_UUID rw quiet splash
     initrd /boot/initramfs-linux.img
+}
+
+menuentry "Velox Linux - NVIDIA (proprietary)" {
+    search --no-floppy --fs-uuid --set=root $ROOT_UUID
+    linux /boot/vmlinuz-linux root=UUID=$ROOT_UUID rw quiet splash nvidia-drm.modeset=1
+    initrd /boot/initramfs-linux.img
+}
+
+menuentry "Velox Linux - NVIDIA (open source)" {
+    search --no-floppy --fs-uuid --set=root $ROOT_UUID
+    linux /boot/vmlinuz-linux root=UUID=$ROOT_UUID rw quiet splash nvidia-drm.modeset=1 nouveau.modeset=1
+    initrd /boot/initramfs-linux.img
+}
+
+menuentry "Velox Linux - AMD" {
+    search --no-floppy --fs-uuid --set=root $ROOT_UUID
+    linux /boot/vmlinuz-linux root=UUID=$ROOT_UUID rw quiet splash amdgpu.modeset=1
+    initrd /boot/initramfs-linux.img
+}
+
+menuentry "Velox Linux (fallback initramfs)" {
+    search --no-floppy --fs-uuid --set=root $ROOT_UUID
+    linux /boot/vmlinuz-linux root=UUID=$ROOT_UUID rw
+    initrd /boot/initramfs-linux-fallback.img
 }
 GRUBEOF
 
