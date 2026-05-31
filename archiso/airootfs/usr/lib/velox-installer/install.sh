ccb84ea141cf84a8019eece90b560daa9cd1a9c2
thelinutubetltos <thelinutubetltos@gmail.com>
2026-05-30 23:35:36 -0700

    fix: simplify to ext4 only, remove btrfs complexity

--- archiso/airootfs/usr/lib/velox-installer/install.sh
+++ archiso/airootfs/usr/lib/velox-installer/install.sh
@@ -13,34 +13,20 @@ sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect microcode modconf kms keyboard k
 # Generate initramfs
 mkinitcpio -p linux
 
-# Get disk info from /proc instead of lsblk (works in chroot)
+# Get disk info
 ROOT_PART=$(findmnt -n -o SOURCE / | sed 's/\[.*\]//')
-ROOT_UUID=$(blkid -s UUID -o value $ROOT_PART)
-ROOT_FS=$(blkid -s TYPE -o value $ROOT_PART)
-
-# Get disk name by stripping partition number from device name
 ROOT_DISK=$(echo $ROOT_PART | sed 's|/dev/||' | sed 's/[0-9]*$//')
-echo "ROOT_PART=$ROOT_PART ROOT_DISK=$ROOT_DISK ROOT_UUID=$ROOT_UUID ROOT_FS=$ROOT_FS"
+ROOT_UUID=$(blkid -s UUID -o value $ROOT_PART)
+echo "ROOT_PART=$ROOT_PART ROOT_DISK=$ROOT_DISK ROOT_UUID=$ROOT_UUID"
 
-# Map filesystem to correct GRUB module name
-case $ROOT_FS in
-    ext2|ext3|ext4) GRUB_FS="ext2" ;;
-    btrfs) GRUB_FS="btrfs" ;;
-    xfs) GRUB_FS="xfs" ;;
-    f2fs) GRUB_FS="f2fs" ;;
-    *) GRUB_FS="ext2" ;;
-esac
-
-# Install grub - support both BIOS and UEFI
+# Install grub
 if [ -d /sys/firmware/efi ]; then
     mkdir -p /boot/efi
-    # Find EFI partition from /proc/partitions
-    EFI_PART=$(blkid | grep vfat | grep ${ROOT_DISK} | awk -F: '{print $1}' | head -1)
-    mount $EFI_PART /boot/efi 2>/dev/null || true
-    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Velox --removable --modules="part_gpt part_msdos $GRUB_FS"
-    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Velox --modules="part_gpt part_msdos $GRUB_FS"
+    mount /dev/${ROOT_DISK}1 /boot/efi 2>/dev/null || true
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Velox --removable
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Velox
 else
-    grub-install --target=i386-pc /dev/$ROOT_DISK --modules="part_gpt part_msdos $GRUB_FS"
+    grub-install --target=i386-pc /dev/$ROOT_DISK
 fi
 
 # Write grub.cfg
@@ -48,7 +34,6 @@ mkdir -p /boot/grub
 cat > /boot/grub/grub.cfg << GRUBEOF
 set default=0
 set timeout=10
-insmod $GRUB_FS
 
 menuentry "Velox Linux" {
     search --no-floppy --fs-uuid --set=root $ROOT_UUID
